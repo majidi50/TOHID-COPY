e2838e3a8b6e4d60915299f776924752ccb17db1
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2025-02-01 22:13:22 +0300

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetad
   m.reply(
     `▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` +
       users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` +
-      '\n└──✪ 𝚃𝙾𝙷𝙸𝙳-𝙰𝙸 ┃ ᴮᴼᵀ ✪──',
+      '\n└──✪ 𝐌𝐒𝐄𝐋𝐀-𝐂𝐇𝐔𝐈-𝐌𝐀𝐒𝐓𝐄𝐑-𝐁𝐎𝐓 ┃ ᴮᴼᵀ ✪──',
     null,
     {
       mentions: users,
