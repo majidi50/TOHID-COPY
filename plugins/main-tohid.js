6bc58bcad8479e9d23a25efc24e0c9b46303f409
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2025-02-01 21:20:04 +0300

    Update main-tohid.js

--- plugins/main-tohid.js
+++ plugins/main-tohid.js
@@ -2,32 +2,32 @@ let handler = async m =>
   m.reply(
     `
 
-≡ © TOHID TECH Groups & Channels 
+≡ © MSELACHUI TECH Groups & Channels 
 
 ─────────────
 ▢ Join WhatsApp Channel For Updates
-https://whatsapp.com/channel/0029VaGyP933bbVC7G0x0i2T
+https://whatsapp.com/channel/0029VakhqAaLtOjBJOL9Wn1q
 
 ▢ Telegram Group 
-https://t.me/Tohid_Tech
+https://t.me/mselachui_Tech
 
 ─────────────
 ≡ Disabled links? Chat Directly! 
 
 ▢ Group WhatsApp 
- https://chat.whatsapp.com/IqRWSp7pXx8DIMtSgDICGu
+ https://chat.whatsapp.com/Bqb6oEUxAneAqxBUBfNdLr
 ─────────────
 ▢ *Owner instagram*
- https://instagram.com/Tohidkhan6332
+ https://instagram.com/mselachui 
 
 ▢ *YouTube*
-• https://www.youtube.com/@Tohidkhan_6332
+• https://www.youtube.com/@mselachui
 
 
 `.trim()
   )
 handler.help = ['ruth']
 handler.tags = ['main']
-handler.command = ['groups', 'channels', 'telegram', 'sgp', 'grp']
+handler.command = ['groups', 'channel', 'chui', 'sgp', 'grp']
 
 export default handler
