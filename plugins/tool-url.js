c763ae9fc043f6f6fab1bd5f4e3e80be12b95caa
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2025-05-30 22:46:49 +0200

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> ©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝚃𝙾𝙷𝙸𝙳_𝙼𝙳 💜`
+      `> ©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝐌𝐒𝐄𝐋𝐀-𝐂𝐇𝐔𝐈-𝐁𝐎𝐓 🐯`
     );
 
   } catch (error) {
