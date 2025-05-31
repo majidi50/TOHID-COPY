427dd2f17edd8c6f6127a8375d38849ab5e018ce
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2025-05-31 09:46:26 +0200

    Update ssweb-tool.js

--- plugins/ssweb-tool.js
+++ plugins/ssweb-tool.js
@@ -31,14 +31,14 @@ async (conn, mek, m, {
     // give credit and use
     const imageMessage = {
       image: { url: screenshotUrl },
-      caption: "*WEB SS DOWNLOADER*\n\n> *©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝚃𝙾𝙷𝙸𝙳_𝙼𝙳*",
+      caption: "*WEB SS DOWNLOADER*\n\n> *©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝐌𝐒𝐄𝐋𝐀-𝐂𝐇𝐔𝐈-𝐁𝐎𝐓*",
       contextInfo: {
         mentionedJid: [m.sender],
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363207624903731@newsletter',
-          newsletterName: "𝐓𝐎𝐇𝐈𝐃 𝐓𝐄𝐂𝐇 🤖",
+          newsletterJid: '120363305273910720@newsletter',
+          newsletterName: "𝐌𝐒𝐄𝐋𝐀-𝐂𝐇𝐔𝐈-𝐓𝐄𝐂𝐇 🐯",
           serverMessageId: 143,
         },
       },
