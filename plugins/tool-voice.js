db11a8d2f1f0a0b0f3daad7b9c9fcb3d0d6277f5
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2025-05-31 10:04:18 +0200

    Update tool-voice.js

--- plugins/tool-voice.js
+++ plugins/tool-voice.js
@@ -73,7 +73,7 @@ async (conn, mek, m, {
 
         // Send menu message with image
         const sentMsg = await conn.sendMessage(from, {  
-            image: { url: "https://i.ibb.co/4ZSYvPTq/lordali.jpg" },
+            image: { url: "https://files.catbox.moe/15vem5.jpg" },
             caption: menuText
         }, { quoted: m });
 
